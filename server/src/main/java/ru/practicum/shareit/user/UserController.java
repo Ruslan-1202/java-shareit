831d7902f02f632e7e8b417f5290e47150a3f7fc
Ruslan Abushaev <rus021281@gmail.com>
2025-03-24 10:58:46 +0300

    Users

--- server/src/main/java/ru/practicum/shareit/user/UserController.java
+++ server/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserCreateDto;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -11,13 +10,11 @@ import ru.practicum.shareit.user.dto.UserDto;
 @RequestMapping(path = "/users")
 @Slf4j
 @RequiredArgsConstructor
-@Validated
 public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserDto create( //@Valid
-                              @RequestBody UserCreateDto userCreateDto) {
+    public UserDto create( @RequestBody UserCreateDto userCreateDto) {
         log.debug("Создание пользователя {}", userCreateDto);
         return userService.create(userCreateDto);
     }
@@ -30,7 +27,6 @@ public class UserController {
 
     @PatchMapping("{id}")
     public UserDto update(@PathVariable Long id,
-//                          @Valid
                           @RequestBody UserDto userDto) {
         log.debug("Обновление пользователя id={}, на пользователя {}", id, userDto);
         return userService.save(id, userDto);
